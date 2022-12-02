bfd47a9c3fef223fdff45a7e6dfe9e07a2397e9c
rishaw <rishawraj0703@gmail.com>
2022-12-02 12:58:19 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import CurrencyRow from "./Components/CurrencyRow";
+import "./App.css";
+
+const URL = "https://v6.exchangerate-api.com/v6/a06f59a57c53a1fa6a70e2be";
+
+const App = () => {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  const [amount, setAmount] = useState(1);
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+  // console.log({ exchangeRate });
+
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
+
+  useEffect(() => {
+    fetch(`${URL}/latest/INR`)
+      .then((res) => res.json())
+      .then((data) => {
+        const firstCurrency = Object.keys(data.conversion_rates)[0];
+        const secondCurrency = Object.keys(data.conversion_rates)[146];
+
+        setCurrencyOptions([...Object.keys(data.conversion_rates)]);
+        setFromCurrency(firstCurrency);
+        setToCurrency(secondCurrency);
+        setExchangeRate(data.conversion_rates[secondCurrency]);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(`${URL}/pair/${fromCurrency}/${toCurrency}`)
+        .then((res) => res.json())
+        .then((data) => setExchangeRate(data.conversion_rate));
+    }
+  }, [fromCurrency, toCurrency]);
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(true);
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(false);
+  }
+
+  return (
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+        onChangeAmount={handleFromAmountChange}
+        amount={fromAmount}
+      />
+      <div className="equals">=</div>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={(e) => setToCurrency(e.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
+      />
+    </>
+  );
+};
+
+export default App;
